fe73f9dd819733edabbaeac698da019c3b3f43d4
nopparat <nopparat.cysit@gmail.com>
2026-01-05 16:08:42 +0700

    feat: update ProductCard and ProductHighlight components to accept props for image source, product name, and price

--- client/src/components/ProductHighlight.jsx
+++ client/src/components/ProductHighlight.jsx
@@ -6,16 +6,9 @@ function ProductHighlight() {
       <div className="container mx-auto">
         <h2 className="text-2xl font-semibold mb-4">Featured Products</h2>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-          {/* 🛠️ Todo 2: ให้ส่งข้อมูลเข้าไปใน Component ProductCard ผ่าน Prop ที่ชื่อว่า `imgSrc`
-            
-            ==== ตัวอย่าง ====
-
-            <ProductCard imgSrc="https://picsum.photos/id/231/200/200" />
-            
-        */}
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          <ProductCard imgSrc="https://picsum.photos/id/231/200/200" title="The Lorem Ipsum for photos. " price="$99.99" />
+          <ProductCard imgSrc="https://picsum.photos/id/232/200/200" title="Wireless Bluetooth Earbuds" price="$79.99" />
+          <ProductCard imgSrc="https://picsum.photos/200/200" title="Stylish Cool Sunglasses in The World" price="$79.99" />
         </div>
       </div>
     </section>
